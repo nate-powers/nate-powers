932ee84f7a139df033bb5c1ab53c7095c6072c0f
nate-powers <73077132+nate-powers@users.noreply.github.com>
2021-07-02 07:03:11 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,3 @@
-var Date = new Date();
-alert("Welcome to my website! " + Date);
-// A date object with an alert displaying a string and a variable displaying the date object.
+
+alert("Welcome to my website!");
+// An alert saying "Welcome to my website!"
