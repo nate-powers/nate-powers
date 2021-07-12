6a6a37bf23ac27a0aa7513e46d279c778b2eb8c0
nate-powers <73077132+nate-powers@users.noreply.github.com>
2021-07-12 12:15:56 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-function expandDesert {
-  var expandedParagraph = "The <span class="desert">Sonoran Desert</span> is a desert found in Arizona, California, and Mexico. It is the hottest desert in Mexico. It has an area of 100,000 sq mi. In the picture, there is a saguaro, native to the <span class="desert">Sonoran Desert</span>";
+function expandedDesert {
+  var expandedParagraph = "The <span class="desert">Sonoran Desert</span> is a desert found in Arizona, California, and Mexico. It is the hottest desert in Mexico. It has an area of 100,000 sq mi. In the picture, there is a saguaro, native to the <span class="desert">Sonoran Desert</span>"
   document.getElementById("desertParagraph").innerHTML = expandedParagraph;
 }
 alert("Welcome to my website!");
