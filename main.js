cf8d3e72a0678fd69dd01e83697aad668277d7ac
nate-powers <73077132+nate-powers@users.noreply.github.com>
2021-12-27 05:07:05 -0700

    Delete main.js

--- main.js
+++
@@ -1,6 +0,0 @@
-function expandedDesert {
-  var expandedParagraph = "The Sonoran Desert is a desert found in Arizona, California, and Mexico. It is the hottest desert in Mexico. It has an area of 100,000 sq mi. In the picture, there is a saguaro, native to the Sonoran Desert";
-  document.getElementById("desertParagraph").innerHTML = expandedParagraph;
-}
-alert("Welcome to my website!");
-// An alert saying "Welcome to my website!"
